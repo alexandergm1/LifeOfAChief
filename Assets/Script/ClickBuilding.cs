3673da6d4d72adc018ab6b57802b80f25ce75cbf
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-02-28 15:46:38 +0000

    add gold to player via click works

--- Assets/Script/ClickBuilding.cs
+++ Assets/Script/ClickBuilding.cs
@@ -7,24 +7,22 @@ public class ClickBuilding : MonoBehaviour
     // Start is called before the first frame update
     public Player player;
     public Castle castle;
-    string testName;
     void Start()
     {
-        testName = "Hi i am a Test";
-        GameObject CastleObject = GameObject.Find("Tower_a");
+        
 
 
        
         
-        Player player = Player.InitializePlayer("Patrick");
-        Castle castle = Castle.InitializeCastle("Edin Castle");
+        player = Player.InitializePlayer("Patrick");
+        castle = Castle.InitializeCastle("Edin Castle");
     }
 
     void OnMouseDown(){
         Debug.Log("Object was clicked");
-        // castle.PayPlayer(player, 10F);
+        castle.PayPlayer(player, 10F);
         Debug.Log(player.Gold);
-        Debug.Log(testName);
+        
 
 
     }
