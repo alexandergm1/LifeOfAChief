61fb90f02f475871903523ffbd77535f7c14ab46
Alexander Gregson-MacLeod <alexander.1gmac@gmail.com>
2022-03-01 14:09:58 +0000

    Player gold and lumber now render on the main game screen

--- Assets/Settings/GameSettings.cs
+++ Assets/Settings/GameSettings.cs
@@ -7,16 +7,19 @@ public class GameSettings : MonoBehaviour
 {
 
     public static Player player;
-    public Text Name;
+    public static Text Name;
     public Text Lumber;
+    public Text Gold;
 
     void Awake(){
         Name.text = player.Name;
         Lumber.text = player.Lumber.ToString();
+        Gold.text = player.Gold.ToString();
     }
 
     void update()
     {
-        
+        Lumber.text = player.Lumber.ToString();
+        Gold.text = player.Gold.ToString();
     }
 }
