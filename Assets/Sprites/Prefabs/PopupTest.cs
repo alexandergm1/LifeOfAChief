d8754109eeac9d422a72258eba78359c3501c278
alexandergm1 <alexander.1gmac@gmail.com>
2022-03-04 11:25:45 +0000

    edit to prefab

---
+++ Assets/Sprites/Prefabs/PopupTest.cs
@@ -0,0 +1,13 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PopupTest : MonoBehaviour
+// {
+//     [SerializeField] private Transform PopupWindowVillage;
+//     [SerializeField] private Transform panel;
+
+//     void Start(){
+//        Instantiate(PopupWindowVillage.transform.SetParent(panel, false));
+//    }
+// }
