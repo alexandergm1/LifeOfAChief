d779d241801059033b1743d873e9d9a7841c6df5
Nixon2223 <93984761+Nixon2223@users.noreply.github.com>
2022-03-08 16:21:45 +0000

    camera fov, rotation

--- Assets/Controllers/HandleObjectsActive.cs
+++ Assets/Controllers/HandleObjectsActive.cs
@@ -6,27 +6,44 @@ public class HandleObjectsActive : MonoBehaviour
 {
     public GameObject[] villages {get; set;}
     public GameObject[] buildingSites {get; set;}
+    public GameObject[] campSites {get; set;}
 
     void Start()
     {
         villages = GameObject.FindGameObjectsWithTag("Village");
         buildingSites = GameObject.FindGameObjectsWithTag("BuildingSite");
+        campSites = GameObject.FindGameObjectsWithTag("CampSite");
     }
 
     void Update()
     {
         foreach (GameObject village in villages)
-        if (Map.Instance.FindVillageByName(village.name) == null)
         {
-        village.SetActive(false);
+            if (Map.Instance.FindVillageByName(village.name) == null)
+            {
+            village.SetActive(false);
+            }
+            else village.SetActive(true);
         }
-        else village.SetActive(true);
 
         foreach (GameObject buildingSite in buildingSites)
-        if (Map.Instance.FindBuildingSiteByName(buildingSite.name) == null)
         {
-        buildingSite.SetActive(false);
+            if (Map.Instance.FindBuildingSiteByName(buildingSite.name) == null || Map.Instance.FindBuildingSiteByName(buildingSite.name).UnderConstruction == true)
+            {
+            buildingSite.SetActive(false);
+            }
+            else buildingSite.SetActive(true);
         }
-        else buildingSite.SetActive(true);
+
+        foreach (GameObject campSite in campSites)
+        {
+            string name = "BuildingSite" + campSite.name.Substring(campSite.name.Length-1);
+            if (Map.Instance.FindBuildingSiteByName(name) == null) campSite.SetActive(false);
+            else if (Map.Instance.FindBuildingSiteByName(name).UnderConstruction == true) campSite.SetActive(true);
+            
+            
+         
+
+        }
     }
 }
