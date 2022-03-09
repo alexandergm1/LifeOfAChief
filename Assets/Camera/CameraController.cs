d02d90876bf3ff76dd8f742db0e8fb294b0c65ad
alexandergm1 <alexander.1gmac@gmail.com>
2022-03-09 17:32:21 +0000

    Added new camera script

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -1,41 +1,37 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    
-    public float panSpeed = 20f;
-    public float panBorderThickness = 10f;
-    public Vector2 panLimit;
+    public float movementSpeed;
+    public float movementTime;
+    public Vector3 newPosition;
 
-    public float scrollSpeed = 20f;
-    public float minY = 20f;
-    public float maxY = 120f;
+    void Start(){
+        newPosition = transform.position;
+    }
 
     void Update(){
+        handleMovementInput();
+    }
 
-        Vector3 pos = transform.position;
+    void handleMovementInput(){
 
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness){
-            pos.z += panSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W)){
+            newPosition += (transform.forward * movementSpeed);
         }
-        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness){
-            pos.x -= panSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S)){
+            newPosition += (transform.forward * -movementSpeed);
         }
-        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness){
-            pos.z -= panSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D)){
+            newPosition += (transform.right * movementSpeed);
         }
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness){
-            pos.x += panSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A)){
+            newPosition += (transform.right * -movementSpeed);
         }
 
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
-
-        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
-
-        transform.position = pos;
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
     }
 
 }
